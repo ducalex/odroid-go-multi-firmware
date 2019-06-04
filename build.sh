50b3a26c9f05ca7227dfed955991274a86c2bd12
Alex Duchesne <alex@netvps.ca>
2019-06-04 18:30:07 -0400

    Changed build to attempt not overwriting the apptable on update

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ BINNAME=odroid-go-firmware
 
 ./tools/mkfw/mkfw "Firmware Manager ($RELEASE)" tile.raw \
     0 32 851968 manager build/$BINNAME.bin \
-    1 254 131072 apptable dummy.bin \
-    0 16 100 dummy dummy.bin
+    1 254 131072 apptable empty.bin \
+    0 16 65536 empty empty.bin
 
 mv firmware.fw "odroid-go-firmware-$RELEASE.fw"
 
@@ -13,5 +13,6 @@ mv firmware.fw "odroid-go-firmware-$RELEASE.fw"
     0x1000 build/bootloader/bootloader.bin \
     0x8000 build/partitions.bin \
     0xf000 build/phy_init_data.bin \
-    0x10000 build/$BINNAME.bin \
-    0xE0000 dummy.bin
+    0x10000 build/$BINNAME.bin #\
+    #0xE0000 dummy.bin
+
